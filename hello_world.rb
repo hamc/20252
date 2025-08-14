b57162989cb0e7d92b2598afd7c754097856f038
hamc <heitor.augusto@gmail.com>
2025-08-13 21:49:41 -0300

    Hello World em Ruby

---
+++ hello_world.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+def main
+  # Função principal que exibe uma mensagem de Hello World
+  puts "Hello, World!"
+  puts "Olá, Mundo!"
+  
+  # Exemplo adicional com interpolação de string
+  nome = "Ruby"
+  puts "Olá, #{nome}!"
+end
+
+# Executa a função principal
+main 
